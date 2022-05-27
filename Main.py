b3c5898b1c71c03e1a7f9b08db7992d3ea3ddd66
BioMikeUkr <77461417+BioMikeUkr@users.noreply.github.com>
2022-05-27 20:50:18 +0300

    Texts changed

--- Main.py
+++ Main.py
@@ -1,10 +1,10 @@
-a = float(input('1 number: '))
+a = float(input('Write 1 number: '))
 u = input('What to do? (+, -. *, /, ^, : )')
 if u =='^':
     u1 = float(input('Який ступінь?:'))
     c = a ** u1
 else:
-    b = float(input('2 number: '))
+    b = float(input('Write 2 number: '))
 if u=='+':
     c=a+b
 if u == '-':
